3dc0e7183d24c2d05849e72dae6080abe931ac0d
MassimoFulvioPezzali <128166025+MassimoFulvioPezzali@users.noreply.github.com>
2024-11-19 11:30:03 +0100

    Add files via upload

---
+++ contact.h
@@ -0,0 +1,15 @@
+#ifndef CONTACT_H
+#define CONTACT_H
+
+#include <string.h>
+#include <iostream>
+
+struct Contact {
+    std::string _surname;
+    std::string _name;
+    unsigned int _tel;
+};
+
+std::ostream &operator<<(std::ostream &os, const Contact &contact);
+
+#endif
